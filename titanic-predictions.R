f2c8d94ca39721f5ec0a2880e6aad8088dcf7b08
Ryan <rwickman@memphis.edu>
2019-11-29 17:06:15 -0600

    Organized calls and removed non SVM approaches

--- titanic-predictions.R
+++ titanic-predictions.R
@@ -1,3 +1,9 @@
+# Load MASS library that contains QDA
+require(MASS)
+require(corrplot)
+require(ROCR)
+require(e1071)
+
 # Make a prediction given a model and test data
 # It will also plot the confusion matrix
 make_prediction = function(pred.mod, test_data)
@@ -5,7 +11,7 @@ make_prediction = function(pred.mod, test_data)
   pred = predict(pred.mod, test_data)
   pred.rounded = round(pred)
   pred.rounded[pred.rounded<0] = 0
-  table(predict=pred.rounded, truth=test_data$Survived)
+  print(table(predict=pred.rounded, truth=test_data$Survived))
   pred
 }
 
@@ -31,16 +37,26 @@ find_best_svm = function(train_data, kernel_type="radial")
     tune.out = tune(svm, Survived ~ .,
                         data=train_data,
                         kernel=kernel_type,
-                        ranges=list(cost=c(0.00001, 0.0001, 0.001, 0.01, 0.1, 1,5,10,100)),
-                        gamma=c(0.5,1,2,3,4))  
+                        ranges=list(cost=c(0.00001, 0.001, 0.01, 0.1, 1,5,10,20,100)),
+                        gamma=c(0.1, 0.5,1,2,3,4, 10,100))  
+  }
+  else if (kernel_type == "polynomial")
+  {
+    tune.out = tune(svm, Survived ~ .,
+                    data=train_data,
+                    kernel=kernel_type,
+                    ranges=list(cost=c(0.00001, 0.001, 0.01, 0.1, 1,5,10,20,100)),
+                    gamma=1,
+                    d=c(0.5,1,2)) 
   }
   else if (kernel_type == "linear")
   {
     tune.out = tune(svm, Survived ~ .,
                          data=train_data,
                          kernel=kernel_type,
-                         ranges=list(cost=c(0.001, 0.01, 0.1, 1,5,10,100)))
+                         ranges=list(cost=c(0.001, 0.01, 0.1, 1, 10)))
   }
+
   summary(tune.out)
   bestmod = tune.out$best.model
   summary(bestmod)
@@ -75,25 +91,20 @@ find_best_num_components = function(train_data, test_data, kernel_type="radial")
   best_num_components
 }
 
-# Load MASS library that contains QDA
-require(MASS)
-require(corrplot)
-require(ROCR)
-require(e1071)
 
 # Load the training and testing data
-setwd("D:\\code\\R\\Titanic-Machine-Learning")
+setwd("D:\\_Code\\R\\Titanic-Machine-Learning")
 train.csv=read.csv("data\\train_new.csv", header=T)
 test.csv.kaggle=read.csv("data\\test_new.csv", header=T)
 
 # Convert the string variable to numeric
-train.csv["Title"] = sapply(train.csv["Title"], as.numeric)
-train.csv["Sex"] = sapply(train.csv["Sex"], as.numeric)
-train.csv["Embarked"] = sapply(train.csv["Embarked"], as.numeric)
+train.csv$Title = sapply(train.csv$Title, as.numeric)
+train.csv$Sex = sapply(train.csv$Sex, as.numeric)
+train.csv$Embarked = sapply(train.csv$Embarked, as.numeric)
 
-test.csv.kaggle["Title"] = sapply(test.csv.kaggle["Title"], as.numeric)
-test.csv.kaggle["Sex"] = sapply(test.csv.kaggle["Sex"], as.numeric)
-test.csv.kaggle["Embarked"] = sapply(test.csv.kaggle["Embarked"], as.numeric)
+test.csv.kaggle$Title = sapply(test.csv.kaggle$Title, as.numeric)
+test.csv.kaggle$Sex = sapply(test.csv.kaggle$Sex, as.numeric)
+test.csv.kaggle$Embarked = sapply(test.csv.kaggle$Embarked, as.numeric)
 
 
 # Find collinearity
@@ -142,13 +153,14 @@ par(mfrow=c(1,2))
 # Data Visualization
 plot(pve, xlab="Principal Component", ylab="Proportion of Variance Explained", ylim=c(0,1), type='b')
 plot(cumsum(pve), xlab="Principal Component", ylab="Cumulative Proportion of Variance Explained ", ylim=c(0,1), type='b')
+
 plot(pca$x[,1]^2, pca$x[,1],col=colors[train.csv$Survived+1])
-plot(pca$x[,1]^0.5, pca$x[,2],col=colors[train.csv$Survived+1])
+plot((pca$x[,1]+10)^0.5, (pca$x[,2]+10),col=colors[train.csv$Survived+1])
 
 train.csv.pca1 = data.frame(x=cbind(pca$x[1:num_train_rows,1]^2, pca$x[1:num_train_rows,1]), Survived=train.csv$Survived)
-train.csv.pca2 = data.frame(x=cbind(pca$x[1:num_train_rows,1]^0.5, pca$x[1:num_train_rows,2]), Survived=train.csv$Survived)
-test.csv.pca2.kaggle = data.frame(x=cbind(pca$x[num_train_rows+1:num_test_rows,1]^0.5, pca$x[num_train_rows+1:num_test_rows,2]))
+train.csv.pca2 = data.frame(x=cbind((pca$x[1:num_train_rows,1]+10)^0.5, pca$x[1:num_train_rows,2]), Survived=train.csv$Survived)
 
+test.csv.pca2.kaggle = data.frame(x=cbind(pca$x[num_train_rows+1:num_test_rows,1]^0.5, pca$x[num_train_rows+1:num_test_rows,2]))
 
 train.csv.pca3 = data.frame(x=pca$x[1:num_train_rows,], Survived=train.csv$Survived)
 test.csv.pca3.kaggle = data.frame(x=pca$x[num_train_rows+1:num_test_rows,])
@@ -158,143 +170,80 @@ percentage_test = 0.2
 num_test_samples = nrow(train.csv) * percentage_test
 
 test_rows = sample(nrow(train.csv), num_test_samples)
-#test.csv = train.csv[test_rows, ]
-#train.csv = train.csv[-test_rows, ]
+test.csv = train.csv[test_rows, ]
+train.csv = train.csv[-test_rows, ]
 
-#test.csv.pca1 = train.csv.pca1[test_rows, ]
-#train.csv.pca1 = train.csv.pca1[-test_rows, ]
+test.csv.pca1 = train.csv.pca1[test_rows, ]
+train.csv.pca1 = train.csv.pca1[-test_rows, ]
 
-#test.csv.pca2 = train.csv.pca2[test_rows, ]
-#train.csv.pca2 = train.csv.pca2[-test_rows, ]
+test.csv.pca2 = train.csv.pca2[test_rows, ]
+train.csv.pca2 = train.csv.pca2[-test_rows, ]
+
+test.csv.pca3 = train.csv.pca3[test_rows, ]
+train.csv.pca3 = train.csv.pca3[-test_rows, ]
 
 
-test.csv.pca3.sub = train.csv.pca3[test_rows, ]
-train.csv.pca3.sub = train.csv.pca3[-test_rows, ]
+## Fit Support Vector Machine (SVM)
+svmfit = svm(Survived ~ Sex + Age + SibSp + Parch + Fare + Embarked + Title, data=train.csv, kernel ="linear", cost=0.01, scale=FALSE)
+summary(svmfit)
 
-num_comp = find_best_num_components(train.csv.pca3.sub, test.csv.pca3.sub)
+svmfit2 = svm(Survived ~ Sex + Age + SibSp + Parch + Fare + Embarked + Title, data=train.csv, kernel ="polynomial", d=2, cost=0.01, gamma=1)
+summary(svmfit)
+
+# Find best regular SVM
+bestmod = find_best_svm(train.csv[, c("Sex", "Age", "SibSp", "Parch", "Fare", "Embarked", "Pclass", "Survived")], "linear")
+summary(bestmod)
+bestmod2 = find_best_svm(train.csv[, c("Sex", "Age", "SibSp", "Parch", "Fare", "Embarked", "Pclass", "Survived")], "radial")
+summary(bestmod2)
+bestmod3 = find_best_svm(train.csv[, c("Sex", "Age", "SibSp", "Parch", "Fare", "Embarked", "Pclass", "Survived")], "polynomial")
+summary(bestmod3)
+
+
+# Find best PCA model using potentially all the components
+num_comp = find_best_num_components(train.csv.pca3, test.csv.pca3)
 bestmod_pca = find_best_svm(train.csv.pca3[,c(1:num_comp, 8)])
 
-
-# Fit Logistic Regression
-#logit.fit = glm(Survived ~ Sex + Age + SibSp + Parch + Fare + Embarked, data=train.csv, family=binomial)
-#logit.fit.all = glm(Survived ~ .- PassengerId - Survived - Ticket - Name - Title - FamSize, data=train.csv, family=binomial)
-
-# Fit Quadratic Discriminant Analysis
-#qda.fit = qda(Survived ~ Sex + Age + SibSp + Parch + Fare + Embarked, data=train.csv)
-#qda2.fit = qda(Survived ~ Sex + Age + SibSp + Parch + Fare + Embarked + Log_Fare, data=train.csv)
-
-# Fit Support Vector Machine (SVM)
-#svmfit = svm(Survived ~ Sex + Age + SibSp + Parch + Fare + Embarked + Log_Fare, data=train.csv, kernel ="linear", cost=10, scale=FALSE)
-#svmfit_pca1 = svm(Survived ~ ., data=train.csv.pca1, kernel ="linear", cost=10, scale=FALSE)
-
-# Find best cost value 
-?svm
-tune.out = tune(svm, Survived ~ Sex + Age + SibSp + Parch + Fare + Embarked + Pclass,
-                data=train.csv,
-                kernel ="radial",
-                ranges=list(cost=c(0.00001, 0.0001, 0.001, 0.01, 0.1, 1,5,10,100)),
-                gamma=c(0.5,1,2,3,4))
-
-summary(tune.out)
-bestmod = tune.out$best.model
-summary(bestmod)
+# Find best using transformed first component
+bestmod_pca1 = find_best_svm(train.csv.pca1, "linear")
 
 
-#tune.out.pca1 = tune(svm, Survived ~ ., data=train.csv.pca1, kernel ="linear", ranges=list(cost=c(0.001, 0.01, 0.1, 1,5,10,100)))
-#summary(tune.out.pca1)
-#bestmod.pca1 = tune.out.pca1$best.model
-#summary(bestmod.pca1)
-
-
-#tune.out.pca2 = tune(svm, Survived ~ .,
-#                     data=train.csv.pca2,
-#                     kernel ="radial",
-#                     ranges=list(cost=c(0.00001, 0.0001, 0.001, 0.01, 0.1, 1,5,10,100)),
-#                     gamma=c(0.5,1,2,3,4))
-#summary(tune.out.pca2)
-#bestmod.pca2 = tune.out.pca2$best.model
-#summary(bestmod.pca2)
-
+# Find best using transformed first two components
+bestmod_pca2 = find_best_svm(train.csv.pca2, "radial")
+train.csv.pca2$x.1
 
 
 ## Cross Evaluation
-# Logistic Regression
-# Cross validation
-#logit.pred = predict(logit.fit, test.csv)
-#logit.class = ifelse(logit.pred>0.5, 1, 0)
-#table(logit.class, test.csv$Survived)
+svm.pred = make_prediction(svmfit, test.csv)
+svm2.pred = make_prediction(svmfit2, test.csv)
+svm.bestmod.pred = make_prediction(bestmod, test.csv)
+svm.bestmod2.pred = make_prediction(bestmod2, test.csv)
+svm.bestmod3.pred = make_prediction(bestmod3, test.csv)
 
-# Using all predictors 
-#logit.pred.all = predict(logit.fit.all, test.csv)
-#logit.class.all = ifelse(logit.pred.all>0.5, 1, 0)
-#table(logit.class.all, test.csv$Survived)
-
-# QDA
-#qda.pred = predict(qda.fit, test.csv)
-#table(qda.pred$class, test.csv$Survived)
-# Adding the log only increases accuracy by a little bit so not worth adding it
-#qda2.pred = predict(qda2.fit, test.csv)
-#table(qda2.pred$class, test.csv$Survived)
-
-## SVM Cross Evaluation
-svm.pred = predict(svmfit, test.csv, values=T)
-svm.pred.rounded = round(svm.pred)
-svm.pred.rounded[svm.pred.rounded<0] = 0
-table(svm.pred.rounded, test.csv$Survived)
-
-#svm_pca.pred = predict(svmfit_pca1, test.csv.pca1, values=T)
-#svm_pca.pred.rounded = round(svm_pca.pred)
-#svm_pca.pred.rounded[svm_pca.pred.rounded<0] = 0
-#table(svm_pca.pred.rounded, test.csv$Survived)
-
-#svm.best.pred = predict(bestmod, test.csv)
-#svm.best.pred.rounded = round(svm.best.pred)
-#svm.best.pred.rounded[svm.best.pred.rounded<0] = 0
-#table(predict=svm.best.pred.rounded, truth=test.csv$Survived)
-
-svm.best.pred.pca1 = predict(bestmod.pca1, test.csv.pca1)
-svm.best.pred.rounded = round(svm.best.pred.pca1)
-svm.best.pred.rounded[svm.best.pred.rounded<0] = 0
-table(predict=svm.best.pred.rounded, truth=test.csv.pca1$Survived)
-
-svm.best.pred.pca2 = predict(bestmod.pca2, test.csv.pca2)
-svm.best.pred.rounded = round(svm.best.pred.pca2)
-svm.best.pred.rounded[svm.best.pred.rounded<0] = 0
-table(predict=svm.best.pred.rounded, truth=test.csv.pca2$Survived)
-
-svm.best.pred.pca3 = predict(bestmod.pca3, test.csv.pca3)
-svm.best.pred.rounded = round(svm.best.pred.pca3)
-svm.best.pred.rounded[svm.best.pred.rounded<0] = 0
-table(predict=svm.best.pred.rounded, truth=test.csv.pca3$Survived)
+svm.bestmod_pca.pred = make_prediction(bestmod_pca, test.csv.pca3)
+svm.bestmod_pca1.pred = make_prediction(bestmod_pca1, test.csv.pca1)
+svm.bestmod_pca2.pred = make_prediction(bestmod_pca2, test.csv.pca2)
 
 ## ROC PREDICTIONS
-#plotROC(logit.pred, test.csv)
-#plotROC(logit.pred.all, test.csv)
-#plotROC(qda.pred$posterior[,2], test.csv)
-#plotROC(qda2.pred$posterior[,2], test.csv)
 plotROC(svm.pred, test.csv)
-plotROC(svm.best.pred, test.csv)
-plotROC(svm.best.pred.pca1, test.csv.pca1)
-#plotROC(svm.best.pred.pca2, test.csv.pca2)
-plotROC(svm.best.pred.pca3, test.csv.pca3)
-
-
-#function = best_svm(train_data, test_data)
-
-
+plotROC(svm2.pred, test.csv)
+plotROC(svm.bestmod.pred, test.csv)
+plotROC(svm.bestmod2.pred, test.csv)
+plotROC(svm.bestmod_pca.pred, test.csv.pca3)
+plotROC(svm.bestmod_pca1.pred, test.csv.pca1)
+plotROC(svm.bestmod_pca2.pred, test.csv.pca2)
 
 ## Kaggle Submission
 # Regular SVM
-svm.pred = predict(bestmod, test.csv.kaggle[,!names(train.csv) %in% omitted_col])
-svm.pred.rounded = round(svm.pred)
-svm.pred.rounded[svm.pred.rounded<0] = 0
-submission = data.frame(PassengerId=test.csv.kaggle$PassengerId,Survived=svm.pred.rounded)
-write.csv(submission, "svm_submission.csv", row.names=FALSE)
+#svm.pred = predict(bestmod, test.csv.kaggle[,!names(train.csv) %in% omitted_col])
+#svm.pred.rounded = round(svm.pred)
+#svm.pred.rounded[svm.pred.rounded<0] = 0
+#submission = data.frame(PassengerId=test.csv.kaggle$PassengerId,Survived=svm.pred.rounded)
+#write.csv(submission, "svm_submission.csv", row.names=FALSE)
 
 # pca svm
-svm.pred = predict(bestmod_pca, test.csv.pca3.kaggle)
-svm.pred.rounded = round(svm.pred)
-svm.pred.rounded[svm.pred.rounded<0] = 0
-submission = data.frame(PassengerId=test.csv.kaggle$PassengerId,Survived=svm.pred.rounded)
-write.csv(submission, "svm_pca_submission.csv", row.names=FALSE)
+#svm.pred = predict(bestmod_pca, test.csv.pca3.kaggle)
+#svm.pred.rounded = round(svm.pred)
+#svm.pred.rounded[svm.pred.rounded<0] = 0
+#submission = data.frame(PassengerId=test.csv.kaggle$PassengerId,Survived=svm.pred.rounded)
+#write.csv(submission, "svm_pca_submission.csv", row.names=FALSE)
 
